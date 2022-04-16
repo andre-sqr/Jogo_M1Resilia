d7d3cf5dfb1c1c180e5a75d4d28e40bb34ae7c38
andre-sqr <andrebsqr@gmail.com>
2022-04-16 13:08:26 -0300

    javascript adicionado

---
+++ index.js
@@ -0,0 +1,23 @@
+function characterChoice() {
+    while (character != 'a' || 'b' || 'c') {
+        var character = prompt(`Qual personagem você deseja escolher?
+        Digite a letra da opção desejada
+        
+        A) John, o xerife
+        B) William, o bandido
+        C) Florence, a fundadora`)
+        
+        if (character.toLowerCase() === 'a') {
+            alert('Você decidiu jogar com John, o xerife')
+            location.href = './html/john/sheriffPhase1.html'
+        } else if (character.toLowerCase() === 'b') {
+            alert('Você decidiu jogar com William, o bandido') 
+            location.href = './html/william/banditPhase1.html'       
+        } else if (character.toLowerCase() === 'c') {
+            alert('Você decidiu jogar com Florence, a fundadora')
+            location.href = './html/florence/civilianPhase1.html'
+        } else {
+            alert('Resposta inválida. Tente novamente')
+        }
+    }
+}
