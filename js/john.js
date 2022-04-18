6a720c44028c7212c44502e315450771efc8962c
andre-sqr <andrebsqr@gmail.com>
2022-04-18 12:45:32 -0300

    fase dois do john adicionada

--- js/john.js
+++ js/john.js
@@ -1,6 +1,6 @@
 // ESCOLHAS
 
-function phaseChoiceOne() {
+function phaseOneChoice() {
     while (answer != 'a' || 'b' || 'c') {
         var answer = prompt(`Qual a sua escolha?
         Digite a letra da opção desejada
@@ -11,7 +11,6 @@ function phaseChoiceOne() {
         
         if (answer.toLowerCase() === 'a') {
             location.href = 'sheriffGameOver101.html' 
-            answer = a
             break
         } else if (answer.toLowerCase() === 'b') { 
             location.href = 'sheriffPhase2.html' 
@@ -25,8 +24,37 @@ function phaseChoiceOne() {
     }
 }
 
+function phaseTwoChoice() {
+    while (answer != 'a' || 'b' || 'c') {
+        var answer = prompt(`Qual a sua escolha?
+        Digite a letra da opção desejada
+        
+        A) Florence
+        B) James
+        C) Começar uma meditação e buscar as respostas em si mesmo`)
+        
+        if (answer.toLowerCase() === 'a') {
+            location.href = 'sheriffPhase3.html' 
+            break
+        } else if (answer.toLowerCase() === 'b') { 
+            location.href = 'sheriffGameOver201.html' 
+            break      
+        } else if (answer.toLowerCase() === 'c') {
+            location.href = 'sheriffGameOver202.html'
+            break
+        } else {
+            alert('Resposta inválida. Tente novamente')
+        }
+    }
+}
+
 // GAME OVER
 
 function goBack() {
+    location.href = '../../index.html'
+}
+
+function jamesKillsJohn() {
+    alert(`Em um movimento súbito, James saca uma pistola que estava escondendo, pressiona contra sua cabeça e atira. Agora, finalmente, ele poderia continuar o caso que começou com a esposa de John, e atraí-lo até a cidade funcionou da forma como havia planejado. Você morre.`)
     location.href = '../../index.html'
 }
