218de175a5636581b023689860711d5587860e10
andre-sqr <andrebsqr@gmail.com>
2022-04-21 02:32:22 -0300

    fase um de florence adicionada

---
+++ js/florence.js
@@ -0,0 +1,39 @@
+function makeChoice(text, rightAnswer, wrongAnswer1, alert1, wrongAnswer2, alert2, link) {
+    while (answer != 'a' || 'b' || 'c') {
+        var answer = prompt(`Qual a sua escolha? \n Digite a letra da opção desejada
+        \n ${text}`)
+        
+        if (answer.toLowerCase() === wrongAnswer1) {
+            alert(alert1)
+            location.href = '../../index.html' 
+            break
+        } else if (answer.toLowerCase() === rightAnswer) { 
+            location.href = link 
+            break      
+        } else if (answer.toLowerCase() === wrongAnswer2) {
+            alert(alert2)
+            location.href = '../../index.html'
+            break
+        } else {
+            alert('Resposta inválida. Tente novamente')
+        }
+    }
+}
+
+function makeChoiceAB (text, rightAnswer, wrongAnswer, alert1, link) {
+    while (answer != 'a' || 'b') {
+        var answer = prompt(`Qual a sua escolha? \n Digite a letra da opção desejada
+        \n ${text}`)
+
+        if (answer.toLowerCase() === rightAnswer) {
+            location.href = link 
+            break
+        } else if (answer.toLowerCase() === wrongAnswer) {
+            alert(alert1)
+            location.href = '../../index.html'
+            break
+        } else {
+            alert('Resposta inválida. Tente novamente')
+        }
+    }
+}
